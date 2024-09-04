cf9fd3d21a4741f8d63dc1efbf61e04f13f1bf85
dheyahamida1510 <99865101+dheyahamida1510@users.noreply.github.com>
2024-09-04 16:08:54 +0700

    update data_scraping.py

--- data_scraping.py
+++ data_scraping.py
@@ -1,10 +1,12 @@
 # deklarasi library yang akan digunakan
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from time import sleep
 import json
 from random import randint
+import re
 
 # untuk password akun LinkedIn
 with open("D:\\Dokumen\\account.txt") as f:
@@ -17,7 +19,12 @@ options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Chrome(executable_path="D:\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe", options=options)
+
+service = Service("D:\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe")
+
+driver = webdriver.Chrome(service=service, options=options)
+
+#driver = webdriver.Chrome(executable_path="D:\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe", options=options)
 
 # membuka halaman login LinkedIn
 driver.get("https://linkedin.com/uas/login")
@@ -45,11 +52,11 @@ sleep(randint(5, 6))
 
 # placeholder data
 link_list = [
-    "https://www.linkedin.com/in/saiku1/",
-    "https://www.linkedin.com/in/tarandeep-singh-947135104/",
+#    "https://www.linkedin.com/in/saiku1/",
+#    "https://www.linkedin.com/in/tarandeep-singh-947135104/",
 #    "https://www.linkedin.com/in/sylvester-kwame-inkoom-phd-6357b560/",
-#    "https://www.linkedin.com/in/mdcochran/",
-#    "https://www.linkedin.com/in/ameya-kabre/",
+    "https://www.linkedin.com/in/mdcochran/",
+    "https://www.linkedin.com/in/ameya-kabre/",
 #    "https://www.linkedin.com/in/maddyrandle/",
 #    "https://www.linkedin.com/in/kevincendana/",
 #    "https://www.linkedin.com/in/-nupurgupta/",
@@ -57,7 +64,8 @@ link_list = [
 #    "https://www.linkedin.com/in/zaretta-hammond-2b122ba/",
 ]
 
-people_data = []
+with open("D:\\Dokumen\\dashboardPro\\people_data.json", "r+") as pd:
+    people_data = json.load(pd)
 
 for dsnt_link in link_list:
 
@@ -154,7 +162,20 @@ for dsnt_link in link_list:
         "experiences" : experience
     }
 
-    people_data.append(profile_dict)
+    if len(people_data) != 0:
+        i = 0
+        match_found = False
+        while i < len(people_data):
+            if re.search(profile_dict["name"], people_data[i]["name"], re.IGNORECASE):
+                people_data[i] = profile_dict
+                match_found = True
+                i = len(people_data)
+            i += 1
+        if match_found == False:
+            people_data.append(profile_dict)
+    else:
+        people_data.append(profile_dict)
+
     sleep(randint(3, 6))
 
 json_data = json.dumps(people_data, indent=3)
